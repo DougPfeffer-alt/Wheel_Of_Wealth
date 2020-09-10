b15ed474c94a716ced4bf1ac2d0642fabd9e3de9
Louis Caruso <carusolouis00@gmail.com>
2020-09-09 21:10:57 -0700

    high score table function

--- hi_scoreLogic.js
+++ hi_scoreLogic.js
@@ -8,17 +8,17 @@ function HiScoree(name, score) {
   hiScoreArray.push(this);
 }
 
-function userMakesHSList(name, score){
+function userMakesHSList(name, score) {
   //adds new user score to array
   new HiScoree(name, score);
 
   //if there is already a hi score array in local storage, use that one instead
-  if(localStorage.getItem('hiScoreArray')){
-  //parses the stringified data back to an array type object
-  hiScoreArray = JSON.parse(localStorage.getItem('hiScoreArray'));
+  if (localStorage.getItem('hiScoreArray')) {
+    //parses the stringified data back to an array type object
+    hiScoreArray = JSON.parse(localStorage.getItem('hiScoreArray'));
   }
   //if there is nothing in local storage, the default hi score list is loaded instead
-  else{
+  else {
     new HiScoree('Nebiyu', 100);
     new HiScoree('Doug', 90);
     new HiScoree('Louis', 80);
@@ -28,15 +28,15 @@ function userMakesHSList(name, score){
     new HiScoree('Bob', 40);
     new HiScoree('Carol', 30);
     new HiScoree('Alice', 20);
-    new HiScoree('Ted', 10);    
+    new HiScoree('Ted', 10);
   }
 
-  //sorts function from lowest score to highest score  
-  hiScoreArray.sort(function(a, b){return a.score - b.score});
+  //sorts function from lowest score to highest score
+  hiScoreArray.sort(function (a, b) { return a.score - b.score;});
 
   //reverses order of array elements to make highest score the first element
   hiScoreArray.reverse();
-  
+
   //there can only be 10 people in the hi score list, so the last is removed
   hiScoreArray.pop();
 }
@@ -46,3 +46,10 @@ userMakesHSList('MKG', 1000);
 
 //console debug
 console.log(hiScoreArray);
+
+//table
+var highScore = document.getElementById('highScoreTable');
+
+function renderHeader() {
+  var 
+}
